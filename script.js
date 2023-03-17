2e1450f46dff04c96e5cfe61fb42e863d757f829
Sujit8102 <117679810+Sujit8102@users.noreply.github.com>
2023-03-17 22:43:48 +0530

    created a submission

--- script.js
+++ script.js
@@ -4,7 +4,9 @@ const person = {
       name: 'John Doe',
       age: 80
     };
-
+const team=player;
+const team1=[...player];
+const cap1={...person};
 // Write your code here, make sure the name of variables is exactly same as mentioned in the problem statement
 
 
